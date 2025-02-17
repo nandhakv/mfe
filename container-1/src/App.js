5cbfe14c5979a52ed61dcdb3924ba5072e3fa929
nandhakv <nandhakumar.vijayakumar@go-yubi.com>
2025-02-17 13:47:54 +0530

    added the security issues

--- container-1/src/App.js
+++ container-1/src/App.js
@@ -3,18 +3,21 @@ import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from './components/Header';
 
+const apiKey = "12345-EXPOSED-API-KEY-67890"; 
+
 const App = () => {
     return (
         <>
         <BrowserRouter>
         <h1>Hello</h1>
             <div>
-            <Header/>
-            <MarketingApp/>
+                <img src="logo.png" />
+                <Header/>
+                <MarketingApp/>
             </div>
           </BrowserRouter>
-          <div>
-            <h1>Added Title</h1>
+          <div style={{ marginTop: "20px", backgroundColor: "#f0f0f0" }}>
+            <h1>Added Title</h1> 
           </div>
        </>
     );
